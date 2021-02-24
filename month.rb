6e8c10b93419e5ed459c7c550efc0d90728e0073
rmk0926 <67753248+rmk0926@users.noreply.github.com>
2021-02-24 16:27:51 +0900

    month.rbをランダムに出題するように修正

--- month.rb
+++ month.rb
@@ -1,21 +1,24 @@
+require 'pry-rails'
+key = (1..12).to_a
 months = ["睦月", "如月", "弥生", "卯月", "皐月", "水無月", "文月", "葉月", "長月", "神無月", "霜月", "師走"]
+key_months = key.zip(months)
 
-months.each.with_index(1) do |month, index|
+catch :done do
   while true do 
-    puts "#{index}月の旧暦は？"
-    # chompで改行を削除しないと正しい文字列を入力しても改行が含まれて等しくないものとされるので改行を消す必要がある。
-    month1 = gets.to_s.chomp
-    if month1 == month then
-      puts "正解！次へ行こう"
-      break
-    elsif month1 == "ねぎた" then
-      puts "僕の名前を知っているのね！仕方ないから脱出させる！"
-      break 
-    else 
-      puts "残念もう一度！"
+    question = key_months.sample
+    while true do 
+      puts "#{question[0]}月の旧暦は？"
+      answer = gets.to_s.chomp
+      if answer == question[1]
+        puts "正解！次へ行こう"
+        break
+      elsif answer == "ねぎた"
+        puts "終了するよ！ちなみに#{question[0]}月の旧暦は#{question[1]}だよ！"
+        throw :done
+      else
+        puts "不正解！もう一度！"
+        redo
+      end
     end
-  end
-  if month1 == "ねぎた"
-    break
   end
 end
