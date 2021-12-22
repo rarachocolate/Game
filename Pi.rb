d55ab483c8f0e88e7e6646eaa4b9c72f0f5f5f4a
rmk0926 <67753248+rmk0926@users.noreply.github.com>
2021-12-22 13:22:01 +0900

    Pi.rbの修正

--- Pi.rb
+++ Pi.rb
@@ -3,11 +3,13 @@ require "bigdecimal"
 circle = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067"
 
 while true do
-  puts "↓円周率を100桁入力してね↓"
+  puts "↓円周率を100桁入力してね↓僕の「誕生日」を入力したら脱出できるよ。"
   input1 = gets.to_s.chomp
   case input1
   when "1112"
     puts "僕の誕生日を知ってるんだね！仕方ないから脱出させてあげる！"
+    puts "ちなみに正解  ↓  "   
+    puts circle
     break
   when circle
     puts "正解！おめでとう！"
